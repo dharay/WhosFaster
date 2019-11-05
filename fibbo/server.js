fcfa34cd3acb8074953aeaaa1caab6893846f70c
Reeya Sengupta <resengupta@deloitte.com>
2019-11-05 16:01:08 +0530

    Adding code for node and java spring

---
+++ fibbo/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+app.get("/", function (req, res) {
+	res.send("Hello World")
+})
+
+function fibboSum(num) {
+	// console.log("value of num in test() " + num)
+	if (num <= 1) return 1;
+	return fibboSum(num - 1) + fibboSum(num - 2);
+}
+
+app.get("/fib/:num", function (req, res) {
+	res.send("Sum: " + fibboSum(req.params.num))
+})
+app.listen(3000)
---
+++ fibonacci/src/main/java/com/example/fibonacci/FibonacciController.java
@@ -0,0 +1,30 @@
+package com.example.fibonacci;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FibonacciController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "/fib/{num}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String getFiboSum(@PathVariable("num") int num) {
+        return "Sum: " + fiboSum(num);
+    }
+
+    private int fiboSum(int num) {
+        if (num <= 1) {
+            return 1;
+        } else {
+            return fiboSum(num - 1) + fiboSum(num - 2);
+        }
+    }
+}
